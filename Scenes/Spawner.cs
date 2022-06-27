17418a8438710adfcd9deb86080f76b965acef54
Nick <61029188+nickadaniel@users.noreply.github.com>
2022-06-26 19:03:51 -0600

    Add files via upload

---
+++ Scenes/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+ 
+    public GameObject[] spawners;
+    public GameObject enemy;
+
+    private void start(){
+        spawners = new GameObject[5];
+
+        for(int i = 0; i < spawners.Length; i++)
+        {
+          //  spawners[i] = transform.GetChild(i).GameObject;
+        }
+    }
+
+    private void Update(){
+        if(Input.GetKeyDown(KeyCode.T)){
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy(){
+        int spawnerId = Random.Range(0, spawners.Length);
+        Instantiate(enemy, spawners[spawnerId].transform.position, spawners[spawnerId].transform.rotation);
+    }
+
+
+}
---
+++ scripts/WelcomeM.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WelcomeM : MonoBehaviour
+{
+  public GameObject UiObject;
+  public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UiObject.SetActive(false);
+    }
+
+     void OnTriggerEnter(Collider enter){
+        UiObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerExit(Collider enter){
+        UiObject.SetActive(false);
+        Destroy(cube);
+    }
+}
